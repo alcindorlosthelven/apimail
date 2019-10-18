2003c4834e124d5e773807d117616bc7ca8cc628
alcindorlos <alcindorlos@gmail.com>
2019-10-18 14:39:32 -0400

    test

--- app/DefaultApp/Routing.php
+++ app/DefaultApp/Routing.php
@@ -3,4 +3,4 @@ use app\DefaultApp\DefaultApp as App;
 App::get("/", "default.index", "index");
 App::post("/", "default.index","index_post");
 App::post("v1/send","v1.send");
-App::get("v1/send","v1.send");
+App::get("v1/send","v1.sendGet");
--- systeme/Model/Mail.php
+++ systeme/Model/Mail.php
@@ -7,6 +7,7 @@
  */
 
 namespace systeme\Model;
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -15,59 +16,50 @@ class Mail
     private static $configuration;
     private static $from;
 
-    public function __construct($configuration=array())
+    public function __construct($configuration = array())
     {
-        self::$configuration=$configuration;
-        self::$from=$configuration['from'];
+        self::$configuration = $configuration;
+        self::$from = $configuration['from'];
 
     }
 
-    private function configuration(){
+    private function configuration()
+    {
         //Server settings
-        try{
+        try {
             $mail = new PHPMailer(true);// Passing `true` enables exceptions
             //$mail->SMTPDebug = 2;
             if (strpos($_SERVER['HTTP_HOST'], "localhost") !== FALSE) {
                 $mail->isSMTP();
             }
-            $mail->Host =self::$configuration['host'];  // Specify main and backup SMTP servers
+            $mail->Host = self::$configuration['host'];  // Specify main and backup SMTP servers
             $mail->SMTPAuth = true;                               // Enable SMTP authentication
             $mail->Username = self::$configuration['utilisateur'];                 // SMTP username
             $mail->Password = self::$configuration['motdepasse'];                           // SMTP password
             $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-            $mail->Port =self::$configuration['port'];
+            $mail->Port = self::$configuration['port'];
             // TCP port to connect to
             return $mail;
-        }catch (Exception $ex){
+        } catch (Exception $ex) {
             throw new Exception($ex->getMessage());
         }
     }
 
-    public function envoyer($to,$sujet,$contenue,$atachement="",$reply=""){
+    public function envoyer($to, $sujet, $contenue, $atachement = "", $reply = "")
+    {
 
         try {
-            $mail=$this->configuration();
-            $mail->setFrom(self::$from['email'],self::$from['nom']);
+            $mail = $this->configuration();
+            $mail->setFrom(self::$from['email'], self::$from['nom']);
 
-            if(is_array($to)){
-                foreach ($to as $m){
-                    if (strpos($m, ",") !== FALSE) {
-                        $t=explode(",",$m);
-                        $mail->addAddress($t[0],$t[1]);
-                    }else{
-                        $mail->addAddress($m);
-                    }
-                }
-            }else{
-                if (strpos($to, ",") !== FALSE) {
-                    $t=explode(",",$to);
-                    $mail->addAddress($t[0],$t[1]);
-                }else{
-                    $mail->addAddress($to);
+            if (is_array($to)) {
+                foreach ($to as $m) {
+                    var_dump($m);
+                    $mail->addAddress($m['email'], $m['nom']);
                 }
             }
 
-            if($atachement != "") {
+            if ($atachement != "") {
                 if (is_array($atachement)) {
                     foreach ($atachement as $attache) {
                         $mail->addAttachment($attache);
@@ -77,25 +69,25 @@ class Mail
                 }
             }
 
-            if($reply != ""){
+            if ($reply != "") {
                 $mail->addReplyTo($reply);
             }
 
-           /*
-            $mail->addCC('cc@example.com');
-            $mail->addBCC('bcc@example.com');*/
+            /*
+             $mail->addCC('cc@example.com');
+             $mail->addBCC('bcc@example.com');*/
 
             //Content
             $mail->isHTML(true);                                  // Set email format to HTML
             $mail->Subject = $sujet;
-            $mail->Body    = $contenue;
+            $mail->Body = $contenue;
 
             //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
             $mail->send();
             return "ok";
         } catch (Exception $e) {
-            return 'Message could not be sent. Mailer Error: '. $mail->ErrorInfo;
+            return 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo;
         }
     }
 
